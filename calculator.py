59a01fb3e3521ff62bcd8bca19eadd224fda968d
adviislam10 <71040123+adviislam10@users.noreply.github.com>
2020-11-03 15:05:35 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# Get data from user
+milesDriven=int (input('Enter miles driven: '))
+milesDriven=float(milesDriven)
+gallonsUsed=int (input('Enter gallons used: '))
+gallonsUsed=float(gallonsUsed)
+
+# Calculation
+mpg=milesDriven/gallonsUsed
+
+# Print output
+print('Your Miles Per Gallon:', mpg)
+
